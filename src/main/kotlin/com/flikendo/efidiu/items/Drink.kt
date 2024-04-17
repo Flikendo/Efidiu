dc57b64805c51327ca46dc6996e421124932f26c
Flikendo <jalasalavidal@gmail.com>
2024-04-17 18:59:41 +0200

    Added vat for food

--- src/main/kotlin/com/flikendo/efidiu/items/Drink.kt
+++ src/main/kotlin/com/flikendo/efidiu/items/Drink.kt
@@ -1,6 +1,7 @@
 package com.flikendo.efidiu.items
 
 import ItemBase
+import org.springframework.beans.factory.annotation.Value
 
 /**
  * This class is used for those items which are drinks
@@ -8,10 +9,23 @@ import ItemBase
 class Drink(private var id: String, private var name: String, private var price: Double, private var vat: Double,
             private var isAlcoholic: Boolean) : ItemBase(id, name, price, vat) {
 
+    @Value("\${flikendo.efidiu.alcoholic.drink.vat}")
+    lateinit var alcoholicDrink: String
+
+    @Value("\${flikendo.efidiu.no.alcoholic.drink.vat}")
+    lateinit var noAlcoholicDrink: String
+
+    /**
+     * Applies alcoholic vat or no alcoholic vat
+     */
     fun applyVat() {
         if (isAlcoholic) {
-            vat
-            this.price *= vat
+            vat = alcoholicDrink.toDouble()
+
+        } else {
+            vat = noAlcoholicDrink.toDouble()
         }
+
+        this.price *= vat
     }
 }
--- src/main/kotlin/com/flikendo/efidiu/items/Food.kt
+++ src/main/kotlin/com/flikendo/efidiu/items/Food.kt
@@ -1,9 +1,22 @@
 package com.flikendo.efidiu.items
 
 import ItemBase
+import org.springframework.beans.factory.annotation.Value
 
 /**
  * This class is used for those items which are food
  */
-class Food(id: String, name: String, price: Double, vat: Double): ItemBase(id, name, price, vat) {
+class Food(private var id: String, private var name: String, private var price: Double,
+           private var vat: Double): ItemBase(id, name, price, vat) {
+    @Value("\${flikendo.efidiu.food.vat}")
+    lateinit var foodVat: String
+
+    /**
+     * Applies vat
+     */
+    fun applyVat() {
+        vat = foodVat.toDouble()
+
+        this.price *= vat
+    }
 }
