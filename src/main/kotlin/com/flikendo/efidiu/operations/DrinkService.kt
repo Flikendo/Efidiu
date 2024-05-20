d659a4c80343d147fe0be9d5bcac2aeb348342b7
Flikendo <jalasalavidal@gmail.com>
2024-05-20 22:37:00 +0200

    Database drink queries

--- src/main/kotlin/com/flikendo/efidiu/operations/DrinkService.kt
+++ src/main/kotlin/com/flikendo/efidiu/operations/DrinkService.kt
@@ -1,20 +1,31 @@
 package com.flikendo.efidiu.operations
 
+import com.flikendo.efidiu.items.Drink
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
 /**
  * This class is used for Mongo CRUD operations
  */
 @Service
-class MongoService {
-    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
+class DrinkService(val db: JdbcTemplate) {
+    /**
+     * Look for all drinks
+     */
+    fun findDrinks(): List<Drink> = db.query("select * from drinks") { response, _ ->
+        Drink(response.getString("id"),
+            response.getString("name"),
+            response.getDouble("price")
+        )
     }
 
-    fun save(message: Message) {
+    /**
+     * Store a drink in DB
+     */
+    fun save(drink: Drink) {
         db.update(
-            "insert into messages values ( ?, ? )",
-            message.id, message.text
+            "insert into drinks values ( ?, ?, ? )",
+            drink.id, drink.name, drink.price
         )
     }
 }
